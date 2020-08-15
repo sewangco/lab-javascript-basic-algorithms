9697428b052518822436b5a14db92a6602c6077d
sewangco <sewangco@gmail.com>
2020-08-15 17:10:11 +0200

    iteration 3

--- js/index.js
+++ js/index.js
@@ -14,3 +14,22 @@ if (hacker1.length > hacker2.length){
   }
   
 // Iteration 3: Loops
+
+let capitalizedName = ''
+for (let i=0; i < hacker1.length; i++){
+  capitalizedName += hacker1[i].toUpperCase() + " ";
+}
+
+let reversedName = ''
+for (let i=hacker2.length -1 ; i >=0 ; i--){
+  reversedName += hacker2[i]
+}
+
+if (hacker1.localeCompare(hacker2)=== -1){
+    console.log(`The driver's name goes first.`)
+  } else if (hacker1.localeCompare(hacker2)=== 1){
+    console.log(`Yo, the navigator goes first definitely.`)
+  } else {
+    console.log(`What?! You both have the same name?`)
+  }
+  
